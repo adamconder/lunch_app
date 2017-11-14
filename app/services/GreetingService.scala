14b3a48176716be3b0beddd4107321d502a02355
Adam Conder <adamdconder@gmail.com>
2017-11-14 11:05:28 +0000

    Add FakeCalenarService interface
    - Testing concrete implementation of GreetingService with a mocked CalendarService interface which mocks out joda calendar

--- app/services/GreetingService.scala
+++ app/services/GreetingService.scala
@@ -1,15 +1,35 @@
 package services
 
-import com.google.inject.ImplementedBy
+import com.google.inject.{ImplementedBy, Inject}
+import org.joda.time.DateTime
 
-class ConcreteGreetingService extends GreetingService {
 
-  def greeting : String = "Alright!"
+@ImplementedBy(classOf[JodaCalender])
+trait CalendarService {
+  def today : DateTime
+}
+
+class JodaCalender extends CalendarService {
+  override def today: DateTime = DateTime.now
+}
+
+class ConcreteGreetingService @Inject()(override val calendarService: CalendarService) extends GreetingService {
+
+  def greeting : String = {
+    val now = calendarService.today
+    val currentHour = now.hourOfDay().get()
+    if (currentHour < 12)
+      "Good morning!"
+    else
+      "Good afternoon!"
+  }
 
 }
 
 @ImplementedBy(classOf[ConcreteGreetingService])
 trait GreetingService {
+
+  val calendarService : CalendarService
 
   def greeting : String
 
--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -1,51 +1,69 @@
 package controllers
 
+import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{status, _}
-import services.GreetingService
+import services.{CalendarService, ConcreteGreetingService, GreetingService}
 
-object FakeGreetingService extends GreetingService {
-  override def greeting: String = "Good morning!"
+object FakeCalendarService extends CalendarService {
+  val formatter : DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
+  override def today: DateTime = DateTime.parse("2017-10-12 12:52:21", formatter)
 }
 
+object MorningCalendarService extends CalendarService {
+  val formatter : DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
+  override def today: DateTime = DateTime.parse("2017-10-12 11:52:21", formatter)
+}
+
+//object FakeGreetingService extends GreetingService {
+//  override def greeting: String = "Good morning!"
+//
+//  override val calendarService: CalendarService = FakeCalendarService
+//}
+//
+//object FakeAfternoonGreetingService extends GreetingService {
+//  override def greeting: String = "Good afternoon!"
+//
+//  override val calendarService: CalendarService = FakeCalendarService
+//}
+
 class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
+
+  val greetingService = new ConcreteGreetingService(FakeCalendarService)
 
   "WelcomeController GET" should {
 
     "return a successful response" in {
-      val controller = new WelcomeController(FakeGreetingService)
+      val controller = new WelcomeController(greetingService)
       val result = controller.welcome.apply(FakeRequest())
       status(result) mustBe OK
     }
 
     "respond to the /welcome url" in {
-
       val request = FakeRequest(GET, "/welcome").withHeaders("Host" -> "localhost")
       val home = route(app, request).get
       status(home) mustBe OK
-
     }
 
     "return some html" in {
-
-      val controller = new WelcomeController(FakeGreetingService)
+      val controller = new WelcomeController(greetingService)
       val result = controller.welcome().apply(FakeRequest())
       contentType(result) mustBe Some("text/html")
-
     }
 
     "say good morning and have a title" in {
 
-      val controller = new WelcomeController(FakeGreetingService)
+      val controller = new WelcomeController(new ConcreteGreetingService(MorningCalendarService))
       val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
       contentAsString(result) must include ("<h1>Good morning!</h1>")
       contentAsString(result) must include ("<title>Welcome!</title>")
     }
 
-    "say good afternoon when it's the afternoon and have a title" ignore {
-      val controller = new WelcomeController(FakeGreetingService)
+    "say good afternoon when it's the afternoon and have a title" in {
+      val controller = new WelcomeController(greetingService)
       val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
       contentAsString(result) must not include ("<h1>Good morning!</h1>")
       contentAsString(result) must include ("<h1>Good afternoon!</h1>")
